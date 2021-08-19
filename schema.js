2cd72c31ef8421f573e96b9f9efda329e2a99d98
SamuraiCheck <63857345+SamuraiCheck@users.noreply.github.com>
2021-08-19 16:49:52 +0300

    Create schema.js

---
+++ schema.js
@@ -0,0 +1,79 @@
+    {
+      "@context": "https://schema.org",
+      "@type": "FAQPage",
+      "mainEntity": [{
+        "@type": "Question",
+        "name": "Is your Firm Licensed and Insured?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "<p>Yes, Element Homes is completely licensed and insured. All subcontractors are fully licensed, bonded and insured. All our subcontractors are also local workers from your city, familiar with local rules and regulations.The state government of California requires all businesses who build homes, commercial buildings or other structures on either private or public land to be licensed by the CSLB. The company’s California Contractors State License Board CSLB License Number is #1058250. You can check out our license through the Department of Consumer Affairs Contractors State License Board website <a href="https://www.cslb.ca.gov/onlineservices/checklicenseII/LicenseDetail.aspx?LicNum=1058250">here</a>.&nbsp;</p>"
+        }
+      }, {
+        "@type": "Question",
+        "name": "How much does it cost to build a custom home in Beverly Hills?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "<p>The cost to build a custom home varies across California because the going rate for labor and materials differs from city to city. Custom homes typically cost more per square foot to construct than model or tract homes. However, upgrades often found in custom homes -- like passive house elements, off-grid capabilities and more -- can actually save the homeowner money in the long run. In our article “<a href="https://www.constructelements.com/post/is-it-cheaper-to-build-or-buy-a-home"><span><strong>Is it Cheaper to Build or Buy a Home</strong></span></a><span>?</span>,” we noted that it might still be cheaper to build a custom home than to buy a pre-built house in California.<br><br>This is because inventory of available homes on the market is low while demand is high. For example, the current cost per square foot for a single family home in Mountain View, California is $986 according to Realtor.com. A custom home might cost anywhere from $300 to $1,000 per square foot to build. Though the cost of building materials like steel, concrete and lumber hit record highs earlier this year, the cost of building materials has since gone down. As such, it could be less expensive to build a custom home in California than to buy one. Learn more about the cost to build or buy a home in your area by searching the locations Element Homes serves.</p>"
+        }
+      }, {
+        "@type": "Question",
+        "name": "How do we start the process of building a custom home?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "<p>Before building your custom home, you must schedule a consultation with the Element Homes team. During this consultation, we will ask a series of questions about your goals for the custom build. We will ask about the lot you plan to build on, what size and layout would work best for your family and what kind of budget you want to work within. After the consultation, our project manager will meet you at your home or build site for a walk through.<br><br>Typically, an Element Homes project manager would visit the site in person. During the site visit, the project manager will assess the site’s condition. After the assessment is complete, we will send over a cost estimate for the project. Once this estimate has been approved, we will design plans for the build and submit these so we can apply for permits. As soon as the permits are approved, we can begin building your dream home!&nbsp;</p>"
+        }
+      }, {
+        "@type": "Question",
+        "name": "Is it hard to get a construction loan for a luxury home?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "<p>It is typically more difficult to secure a construction loan for a luxury home than to secure a mortgage for a pre-built home. In his article “<a href="https://www.nerdwallet.com/article/mortgages/buy-a-house-that-hasnt-been-built-yet"><strong>How to Get a Construction Loan to Build a Home</strong></a>” for NerdWallet, Certified Financial Planner Hal M. Bundrick explains. Bundrick notes that banks are more selective when approving construction loans because they are “taking extra risk during the building phase, since there isn’t an asset to secure the mortgage.” However, notes mortgage loan officer Jerry Thomas, “‘if you can put 20% down and you have a 720 credit score or better, you know you're pretty much going to qualify for everybody's program.’”<br><br>Types of Construction Loans in California<br><br>According to Kiah Treece and Mike Cetera in their article “<a href="https://www.forbes.com/advisor/mortgages/construction-loans/"><span><strong>Your Guide To Construction Loans</strong></span></a>” for Forbes, there are four types of construction loans available to California homeowners. These include construction-to-permanent loans, construction-only loans, owner-builder loans and renovation loans. Treece and Cetera write that a construction-to-permanent loan “finances construction of a home and then converts into a fixed-rate mortgage once the home is completed.” With a construction-only loan, the lender “issues a short-term, adjustable-rate loan that is used to complete construction of a home.” Once the home has been built, “the loan must be paid in full or refinanced into a mortgage.”<br>&zwj;<br>Owner-builder loans are only issued to owners who plan to act as the general contractor for their build. As such, owner-builder loans are “only available to owners who can demonstrate experience as a homebuilder or have a contractor’s license.” Last on our list is the renovation loan. Cetera and Treece write that renovation loans are “most akin to a traditional mortgage” because they are “based on the anticipated value of the home after renovations.” There are quite a few home improvement loans available to California homeowners. These include cash-out refinancing, FHA 203(k) rehab loans, home equity loans, home equity lines of credit and personal loans<br></p>"
+        }
+      }, {
+        "@type": "Question",
+        "name": "What is the typical timeline for a custom home in Beverly Hills? ",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text":"<ul><li><p>Availability of Necessary Building Materials</p></li><li><p >Access to the Site</p></li><li><p>Condition and Availability of Utilities</p></li><li><p>Slope or Grade of the Build Site</p></li><li><p>Local Building Codes -- Especially Wildland Building Codes</p></li><li><p>Local Health and Safety Ordinances</p></li><li><p>Social Distancing Requirements</p></li><li><p>Other Restrictions Related to COVID-19&nbsp;</p></li><li><p>Weather and Other Environmental Conditions</p></li><li><p>Labor Shortages</p></li><li><p>Permitting Delays</p></li><li><p>Resistant Neighbors</p></li><li><p>Recent Changes to Local Building Ordinances&nbsp;</p></li></ul><p>The good news is that working with a design-build firm ensures your entire project is completed under a single contract. This means that we at Element Homes can provide you with one timeline and budget estimate that we promise to stick to. If there are any delays or added expenses, we will notify you immediately.<br></p><p>The timeline for building a custom home in any city or neighborhood in California can vary widely. There are a number of elements that can impact a build timeline, from the time it takes to source certain building materials to the availability of utilities at the site. Ease of accessibility to the site can also extend the build timeline. In her article “How Long Does It Take to Build a House?” for Realtor.com, Margaret Heidenry writes that a “ballpark average time for <strong>building a new home is four months</strong> if you have the pedal to the metal.” She identifies the keys to successfully building a new home as “having approved house building permits, a process that can take a long time in some areas.”<br><br>Elements that Might Impact the Build Timeline<br></p>"}
+        },
+        {
+        "@type": "Question",
+        "name": "In which areas do you build custom homes?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "<p>Element Homes designs and builds custom homes across Northern and Southern California, often in the San Francisco Bay and Greater Los Angeles Areas. The neighborhoods we serve in Northern California include Oakland, Mountain View, Brentwood, Napa, San Jose, San Mateo, Fremont, Los Altos and Saratoga. In Southern California, we build homes in Beverly Hills, Long Beach, Anaheim, Santa Ana, Irvine, San Bernardino, Oxnard, Glendale, Pasadena, Manhattan Beach, Newport Beach, Malibu, Laguna Beach, Calabasas, West Hollywood, Laguna Niguel, Santa Monica and Dana Point.</p>"
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "How much ‘downpayment’ do you typically require and how do you schedule your progress payments? ",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "<p>Element Homes does not rely on traditional payment processes when charging clients because these systems do not make sense for a custom home build. Instead, we utilize a milestone-based payment system. This way, you only pay for the finished job. Rather than placing a large down payment or paying in monthly installments, you pay when work is done, verified and approved by inspectors. <a href="https://youtu.be/RYov4o1of-s"><strong>Watch this video</strong></a> about what makes Element Homes different to learn more.</p>"
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "Who will assist me in the permit process and How much will it cost to get through the permitting process in Beverly Hills ?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "<p>Because our team is local to California, Element Homes understands the permitting process. Our team members Rahul -- Head of Operations -- and Rucha -- Head of Architecture &amp; Planning -- can answer any questions you might have about the permitting process. Rahul has a Masters degree in Civil Engineering while Rucha has almost a decade of experience in the industry. Rucha is always up-to-date with local ordinances, government regulations and city requirements.&nbsp;<br><br>As for the cost of permitting, this might vary somewhat depending on your area. However, <a href="https://www.homeadvisor.com/cost/architects-and-engineers/get-a-building-permit/"><strong>HomeAdvisor’s 2021 Cost Guide</strong></a> for building permits estimates most homeowners will spend between $423 - $2,264. You can find out more about the cost of building permits in the San Francisco Bay Area by visiting the San Francisco Department of Building Department website <a href="https://sfdbi.org/fees"><strong>here</strong></a>. If you plan to build in the Greater Los Angeles Area, you can find out more about the cost of permits from the City of LA Department of Building and Safety website <a href="https://www.ladbs.org/services/pay-fees/permit-fee-calculator"><strong class="bold-text-6">here</strong></a>. According to the <a href="https://pw.lacounty.gov/general/faq/index.cfm?Action=getAnswers&amp;FaqID=JCFNXzNTUCAgCg==&amp;Theme=&amp;ShowTemplate="><strong>LA County Department of Public Works</strong></a>, homeowners must apply for “building, electrical, plumbing and mechanical permits [in order] to build a new house.” Additional permits like grading permits might also be required depending on the type of build.<br></p>"
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "Do you warranty the quality of your materials and constructions?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "<p>The length of warranty for materials used during construction depends on the company, brand or retailer from which we source individual products. As such, length and coverage of these warranties will differ from product to product.<br></p>"
+        }
+      },
+      {
+        "@type": "Question",
+        "name": "Who will be assigned to my project, and how often will I get project updates?",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": "<p>A dedicated project manager and qualified design-build team will be assigned to your project. Through our Live Project Tracking app, you can track your project at any moment. Our Live Project Tracking app allows you to make all your materials selections, review project photos and track your schedule and projects running total from your phone or computer. You can also contact your project manager for updates.&nbsp;<br><br></p>"
+        }
+      },]
+    }
